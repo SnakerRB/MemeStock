790425928fdd603b37c3dfd22ed118e51e72ab76
SnakerRB <dr6887762@gmail.com>
2025-04-20 23:30:32 +0200

    rework

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-700 flex items-center justify-center">
+      <h1 className="text-white text-5xl font-bold">MemeStock funcionando con Tailwind 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
+
